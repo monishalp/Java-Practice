3559017d36059c9fe4fa2ace7b94036056068bc7
Monisha Lakshmipathi <monisha4155@gmail.com>
2016-07-20 19:30:35 -0400

    To find the Average value in an array

---
+++ Average_Array.java
@@ -0,0 +1,17 @@
+public class Average {
+	
+
+		public static void main(String[] args) {
+			int numbers[]={11,22,33,44,55,66,77,88,99,100};
+			int sum=0;
+			for(int i=0;i<numbers.length;i++)
+			{
+				sum=sum+numbers.length;
+			}
+	double average=  sum/numbers.length;
+	System.out.println("Average:" +average);
+		}
+	
+
+	}
+
